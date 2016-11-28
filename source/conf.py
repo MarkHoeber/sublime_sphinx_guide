594dd00f81137f6e0fa43a4621a05cb32461e840
Mark Hoeber <mhoeber@cimpress.com>
2016-11-27 20:52:39 -0500

    styles

--- source/conf.py
+++ source/conf.py
@@ -4,8 +4,8 @@ import sphinx_bootstrap_theme
 
 extensions = []
 
-#templates_path = ['_templates']
-#html_static_path = ["_static"]
+templates_path = ['_templates']
+html_static_path = ['_static']
 
 source_suffix = '.rst'
 
